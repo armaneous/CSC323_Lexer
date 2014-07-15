715de229f2dc10c37c129a98c9982eb162366ac5
x2adrew <armaneous@gmail.com>
2014-07-15 10:27:11 -0700

    Added file reader

---
+++ src/edu/fullerton/csc323/lex/Reader.java
@@ -0,0 +1,38 @@
+package edu.fullerton.csc323.lex;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Reader {
+	private BufferedReader br;
+	private String line;
+
+	public Reader(File file){
+		try {
+			br = new BufferedReader(new FileReader(file));
+		} catch (FileNotFoundException e) {
+			System.out.println("ERROR: File does not exist.");
+		}
+	}
+	
+	public String nextLine() {
+		try {
+			if((line = br.readLine()) != null)
+				return line;
+		} catch (IOException e) {
+			System.out.println("ERROR: " + e.getStackTrace());
+		}
+		return null;
+	}
+	
+	public void closeReader(){
+		try {
+			br.close();
+		} catch (IOException e) {
+			System.out.println("ERROR: " + e.getStackTrace());
+		}
+	}
+}
